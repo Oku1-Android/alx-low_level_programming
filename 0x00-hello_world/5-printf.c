8bcd4182c292ea18ea94a0a4b9ec328d2985bd13
Oku1-Android <imehoku1522@gmail.com>
2023-02-17 07:24:17 -0800

    printf program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *
+ * printf line of string
+ *
+ * return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return 0;
+}
