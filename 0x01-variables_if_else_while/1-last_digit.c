3d989669804c77e3b3274242215ef7fc3cd0e326
Oku1-Android <imehoku1522@gmail.com>
2023-02-18 12:57:52 -0800

    printing last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 
 	if (x > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5 ", n, x);
+		printf("Last digit of %d is %d and is greater than 5", n, x);
 	}
 	if (x == 0)
 	{
-		printf("Last digit of  %d is %d and is 0", n, x);
+		printf("Last digit of %d is %d and is 0", n, x);
 	}
 	if (x < 6 && x != 0)
 	{
