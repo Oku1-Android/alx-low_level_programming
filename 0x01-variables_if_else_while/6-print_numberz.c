0e548c42518d4bebfc5e8b9c15e3b71a2a153f7b
Oku1-Android <imehoku1522@gmail.com>
2023-02-18 15:39:43 -0800

    base 1o digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int n = 0;
+	
+	for (n = 0; n < 10; n++)
+         {
+                putchar("%d", n);
+                n++;
+	 }
+                putchar("\n");
+                return (0);
+}
