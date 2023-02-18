6fd02248955d559ae080dd36b3f573e54438ea8a
Oku1-Android <imehoku1522@gmail.com>
2023-02-18 12:51:31 -0800

    printing last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	x = n % 10;
-	printf("Last digit of %d is %d ", n, x);
+
 	if (x > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5 ", n, x);
