b3141cb546f7dac41fd25eebede888ce011c4592
Oku1-Android <imehoku1522@gmail.com>
2023-02-18 17:07:46 -0800

    putchaar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- *main -> assign a random number to the variable n each time it is executed
- *and print the last digit of the number stored in the variable n
- *Return: Always 0 (Success)
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int n;
-		for (n = 0; n < 10; n++)
-	
-		putchar((num % 10) + '0');
-		
-		putchar("\n");
+	int num;
 
-		return (0);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
 }
