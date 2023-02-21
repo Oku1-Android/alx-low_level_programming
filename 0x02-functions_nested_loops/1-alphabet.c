7edc99097b378008eeedda1f16ff5c35b3e2f38d
Oku1-Android <imehoku1522@gmail.com>
2023-02-21 04:07:55 -0800

    letters of the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet(void);
+ * A code written by Oku Imeh
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char u) 
+int _putchar(char u);
+void print_alphabet(void);
 
 #endif
