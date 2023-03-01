e910f17ca3590fbf56e402a44ee5c06224282baa
Oku1-Android <imehoku1522@gmail.com>
2023-02-28 16:52:20 -0800

    resetiing a pointer value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *reset_to_98 - reeting a pointer
+ *reset_to_98 - reading a pointer
  *@n: character to reset
  *Return: 0
  */
 
 void reset_to_98(int *n)
 {
-	int b;
-
-	n = &b;
-	n = 98;
-	_putchar('0' + n);
-
-	return (0);
+	*n = 98;
 }
