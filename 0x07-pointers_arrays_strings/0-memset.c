03cc30b609c19aed42c05c0d600a73645f7d29c6
Oku1-Android <imehoku1522@gmail.com>
2023-03-14 16:33:53 -0700

    copy memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,25 @@
-#include "main"
+#include "main.h"
 
 /**
- * _memset - the memory set
- * @s: the memory area
- * @n: the number of bytes
- * @b: the bytes to fill the memory
- *Return: s
+ * _memset - function fill the first @n bytes of the memory area pointed
+ * to by @s with the constant byte @b
+ *
+ * @n: bytes of the memory area pointed to by @s
+ *
+ * @s: with the constant byte @b
+ *
+ * @b: memory area pointer
+ *
+ * Return: a pointer to the memory area @s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i = n; i++)
+	while (i < n)
 	{
-		s[i] = b;
+	s[i] = b;
+	i++;
 	}
 
 	return (s);
